bda12c5fc93ea037b4fed5c3c5176d4661cd8835
ShahadNasser <Shahad.N.Alhanaya@gmail.com>
2021-10-02 15:43:34 +0300

    changes

--- app/src/main/java/com/example/guessthephrase/MainActivity.kt
+++ app/src/main/java/com/example/guessthephrase/MainActivity.kt
@@ -7,9 +7,12 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,12 +23,16 @@ class MainActivity : AppCompatActivity() {
     lateinit var promptTextView: TextView
     lateinit var phraseTextView: TextView
     lateinit var guessedLettersTextView: TextView
-    lateinit var messageArrayList: ArrayList<String>
-    val phrase: String = "Hello World"
-    var phraseDisplayed = ""
-    var phraseGuess = 10
-    var lettersGuess = 10
 
+    var phrase = "Hello World"
+    val phraseDictionary = mutableMapOf<Int, Char>()
+    private var userGuess = ""
+    private var userLetterGuess = ""
+    private var count = 0
+    var phraseStage = true
+
+
+    lateinit var messageArrayList: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,65 +48,127 @@ class MainActivity : AppCompatActivity() {
 
 
         messageArrayList = ArrayList()
-        phraseDisplayed = updateDisplayedPhrase("")
-
-        phraseTextView.text = phraseDisplayed
-        playAgainButton.isVisible = false
-        guessEditText.hint = "Guess the full phrase"
-        promptTextView.text = "Guess the full phrase"
-
-        remainingAttempts_textview.text="Remaining Attempts: $phraseGuess"
-
 
         messages_recyclerView.adapter = MessagesRecyclerViewAdapter(messageArrayList)
         messages_recyclerView.layoutManager = LinearLayoutManager(this)
 
-        guessButton.setOnClickListener {
-            val userGuess = guessEditText.text.toString()
-            if (userGuess.isNotEmpty() || userGuess.trim() != "") {
-                if (phraseGuess > 0) {
-                    if (userGuess.lowercase() == phrase.lowercase()) {
-                        Snackbar.make(mainActivityLayout, "you won", Snackbar.LENGTH_SHORT)
-                            .show()
-                    } else {
-                        phraseGuess--
-                        remainingAttempts_textview.text="Remaining Attempts: $phraseGuess"
-                        messageArrayList.add("your guess is $userGuess \nwrong guess")
-                        messages_recyclerView.adapter?.notifyItemInserted(messageArrayList.size)
-                    }
-                }else if(lettersGuess > 0 ){
-                    guessEditText.hint = "Guess the a letter"
-                    promptTextView.text = "Guess the a letter"
-                    if(phrase.contains(userGuess,true)){
-                        phraseDisplayed = updateDisplayedPhrase(userGuess)
-                        phraseTextView.text = phraseDisplayed
-                    }else{
-                        lettersGuess--
-                        remainingAttempts_textview.text="Remaining Attempts: $lettersGuess"
-                        messageArrayList.add("your guess is $userGuess \nwrong guess")
-                        messages_recyclerView.adapter?.notifyItemInserted(messageArrayList.size)
-                    }
-                }
+        playAgainButton.isVisible = false
+        guessedLettersTextView.isVisible = false
+
+        guessButton.setOnClickListener { addMessage() }
+
+        for (i in phrase.indices) {
+            if (phrase[i] == ' ') {
+                phraseDictionary[i] = ' '
+                userGuess += ' '
             } else {
-                Snackbar.make(mainActivityLayout, "Please enter your guess", Snackbar.LENGTH_SHORT)
-                    .show()
+                phraseDictionary[i] = '*'
+                userGuess += '*'
             }
         }
+        updateText()
 
 
     }
 
-    private fun updateDisplayedPhrase(userGuess: String): String {
-        var updatedPhrase = phraseDisplayed
-        for (character in phrase) {
-            updatedPhrase += when {
-                character.toString().equals(userGuess, true) -> character
-                character == ' ' -> ' '
-                else -> '*'
+    private fun addMessage() {
+        var guess = guessEditText.text.toString().lowercase()
+
+        if (phraseStage) {
+            if (guess == phrase.lowercase()) {
+                showDialog("You won!", "you guessed correctly!")
+                guessEditText.isVisible = false
+                guessButton.isVisible = false
+                playAgainButton.isVisible = true
+            } else {
+                messageArrayList.add("Wrong guess: $guess")
+                phraseStage = false
+                updateText()
+            }
+        } else {
+            if (guess.isNotEmpty()) {
+                userGuess = ""
+                checkLetters(guess)
+            } else {
+                Snackbar.make(
+                    mainActivityLayout,
+                    "Please enter one letter at least",
+                    Snackbar.LENGTH_LONG
+                ).show()
             }
         }
-        return updatedPhrase
+        guessEditText.text.clear()
+        guessEditText.clearFocus()
+        messages_recyclerView.adapter?.notifyDataSetChanged()
     }
 
+    private fun checkLetters(guess: String) {
+        val guessedLetter = guess.first()
+        var found = 0
+        for (i in phrase.indices) {
+            if (phrase[i] == guessedLetter) {
+                phraseDictionary[i] = guessedLetter
+                found++
+            }
+        }
+        for (i in phraseDictionary) {
+            userGuess += phraseDictionary[i.key]
+        }
+
+        if (userGuess.lowercase() == phrase.lowercase()) {
+            showDialog("You won!", "you guessed correctly!")
+            guessEditText.isVisible = false
+            guessButton.isVisible = false
+            playAgainButton.isVisible = true
+        }
+            if (userLetterGuess.isEmpty()) {
+                userLetterGuess += guessedLetter
+            } else {
+                "$userLetterGuess,  $guessedLetter"
+            }
+            if (found > 0) {
+                messageArrayList.add("Found $found ${guessedLetter.toUpperCase()}(s)")
+            } else {
+                messageArrayList.add("No ${guessedLetter.toUpperCase()}s found")
+            }
+            count++
+            val guessesLeft = 10 - count
+            if (count < 10) {
+                messageArrayList.add("$guessesLeft guesses remaining")
+            }
+
+        updateText()
+        messages_recyclerView.scrollToPosition(messageArrayList.size - 1)
+    }
+
+    private fun updateText() {
+        phraseTextView.text = "Phrase:  $userGuess"
+        guessedLettersTextView.text = "Guessed Letters:  $userLetterGuess"
+        if (phraseStage) {
+            guessEditText.hint = "Guess the full phrase"
+            promptTextView.text = "Guess the full phrase"
+
+        } else {
+            guessedLettersTextView.isVisible = true
+            promptTextView.text = "Guess a letter"
+            guessEditText.hint = "Guess a letter"
+        }
+    }
+
+
+    private fun showDialog(title: String, message: String) {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage("$message\nDo you want to play again?")
+            .setCancelable(false)
+            .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id ->
+                this.recreate()
+            })
+            .setNegativeButton("No", DialogInterface.OnClickListener { dialog, id ->
+                dialog.cancel()
+            })
+        val alert = dialogBuilder.create()
+        alert.setTitle(title)
+        alert.show()
+    }
 
 }
